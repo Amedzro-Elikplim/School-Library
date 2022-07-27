6c76d55cc009f2319d0718f86ccff1eab00e9964
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-07-27 18:33:43 +0000

    Create constructor with parameters

---
+++ Person.rb
@@ -0,0 +1,12 @@
+class Person
+    
+  def initialize(name = 'Unknown', age, parent_parameters = true) 
+    @id = Random.rand(1..1000)
+    @name = name
+    @age = age
+    @parent_parameters = parent_parameters
+  end
+
+  
+
+end
