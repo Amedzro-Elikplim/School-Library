58b35a4bb0b93d3650af89f0e558a46b988617c1
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-07-27 19:20:09 +0000

    Create constructor

---
+++ Teacher.rb
@@ -0,0 +1,12 @@
+require_relative 'Person'
+
+class Teacher < Person
+
+    def initialize(name = 'Unknown', age, parent_permission: true, specialization)
+       super(name, age, parent_permission)
+       @specialization = specialization
+    end
+
+   
+end
+
