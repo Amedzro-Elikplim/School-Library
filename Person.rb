dd863f7de981d4f4f5045b5ce64e4e96876168c2
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-07-27 18:39:53 +0000

    Add getters and setters

--- Person.rb
+++ Person.rb
@@ -1,5 +1,5 @@
 class Person
-    
+  #constructor
   def initialize(name = 'Unknown', age, parent_parameters = true) 
     @id = Random.rand(1..1000)
     @name = name
@@ -7,6 +7,29 @@ class Person
     @parent_parameters = parent_parameters
   end
 
-  
+  #id getter
+  def getId
+    @id
+  end
+
+  #name getter
+  def getName
+    @name
+  end
+
+  #age getter
+  def getAge
+    @age
+  end
+
+  #name setter
+  def setName = (name)
+    @name = name
+  end
+
+  #age Setter
+  def setAge = (age)
+    @age = age
+  end
 
 end
