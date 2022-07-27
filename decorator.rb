4362f72567349f24db9693f5260ba6fdbdd01c0d
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-07-27 23:27:04 +0000

    Create Capitalize decorator

--- decorator.rb
+++ decorator.rb
@@ -1,12 +1,39 @@
 require_relative 'nameable'
 
 class BaseDecorator < Nameable
-    def initialize(nameable)
-      super()
-      @nameable = nameable
-    end
+  def initialize(nameable)
+    super()
+    @nameable = nameable
+  end
 
-    def correct_name
-      @nameable.correct_name
-    end
+  def correct_name
+    @nameable.correct_name
+  end
 end
+
+
+class CapitalizeDecorator < BaseDecorator
+  def initialize(nameable)
+    super()
+    @nameable = nameable
+  end
+
+  def correct_name
+    output = @nameable.correct_name
+    output.upcase
+  end
+end
+
+class CapitalizeDecorator < BaseDecorator
+  def initialize(nameable)
+    super()
+    @nameable = nameable
+  end
+
+  def correct_name
+    output = @nameable.correct_name
+    output.upcase
+  end
+end
+
+
