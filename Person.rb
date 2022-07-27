465a160ffafe9647b3f6315d1744cbcc4db1adee
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-07-27 19:05:53 +0000

    Fix boolean syntax error

--- Person.rb
+++ Person.rb
@@ -1,6 +1,6 @@
 class Person
   #constructor
-  def initialize(name = 'Unknown', age, parent_permission = true) 
+  def initialize(name = 'Unknown', age, parent_permission: true) 
     @id = Random.rand(1..1000)
     @name = name
     @age = age
