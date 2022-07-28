57d0947ccb632c61dbd8f636aaa36b273ea04e64
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-07-28 00:09:34 +0000

    Capitalize output

--- decorator.rb
+++ decorator.rb
@@ -29,4 +29,4 @@ person.correct_name
 capitalized_person = CapitalizeDecorator.new(person)
 capitalized_person.correct_name
 capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
-print capitalized_trimmed_person.correct_name
+capitalized_trimmed_person.correct_name
