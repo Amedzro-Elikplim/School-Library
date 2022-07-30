dad1552344b93a306413fda04fce9838ce89ff09
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-07-30 22:32:32 +0000

    Use rental as intermediate

--- rental.rb
+++ rental.rb
@@ -1,10 +1,21 @@
 require_relative 'book'
 require_relative 'person'
 
+attr_accessor :date :book, :person
+
 class Rental
   def initialize(date)
     @date = date
+    @book = book
+    @person = person
   end
 
-  attr_accessor :date
+
+  def add_rental
+    book.rentals << self
+  end
+
+  def add_to_person
+    person.rentals << self
+  end
 end
