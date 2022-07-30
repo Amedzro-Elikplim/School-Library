07a99f2c4073c92cdf50460a079636e3b545e630
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-07-30 23:44:46 +0000

    Add method to add books

--- app.rb
+++ app.rb
@@ -11,4 +11,14 @@ class App
     @people = []
     @rentals = []
   end
+
+   def add_book
+    print 'Title:'
+    title = gets.chomp
+    print 'Author:'
+    author = gets.chomp
+    book = Book.new(title, author)
+    puts 'Book created successfully'
+    @books << book
+  end
 end
