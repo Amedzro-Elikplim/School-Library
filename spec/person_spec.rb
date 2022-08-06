2164ea1b30e2890f34253162ab90b2333034da3a
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-08-06 15:20:31 +0000

    Test instantiation of Person object

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -6,6 +6,11 @@ before :each do
     @person = Person.new 'age', 'Name', 'Parent_permission'
 end
 
+ describe '#new' do
+    it 'takes three parameters and returns a Person object' do
+      @person.should be_an_instance_of Person
+    end
+  end
 
 
 end
