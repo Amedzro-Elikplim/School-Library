952678544cd7f45196f4f8380e8be3da6286f6c4
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-08-06 17:13:31 +0000

    Fix linter error

--- person.rb
+++ person.rb
@@ -4,7 +4,7 @@ class Person < Nameable
   attr_reader :id
   attr_accessor :name, :age, :rentals
 
-  def initialize(age, name = 'Unknown', parent_permission)
+  def initialize(age, name, parent_permission)
     super()
     @id = Random.rand(1..1000)
     @age = age
