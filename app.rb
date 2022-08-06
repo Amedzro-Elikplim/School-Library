d58684900a4e21f2673c1ecd71b879835a1b3f63
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-08-06 13:41:16 +0000

    Import storage and json

--- app.rb
+++ app.rb
@@ -2,6 +2,8 @@ require_relative 'book'
 require_relative 'student'
 require_relative 'teacher'
 require_relative 'rental'
+require_relative './storage'
+require 'json'
 
 class App
   attr_reader :books, :people, :rentals
