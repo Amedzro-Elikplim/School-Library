c3bafb5dd3bcc60b0ef66b6e5525f8e25cbd0ef1
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-08-06 16:21:41 +0000

    Test for date and book returned

--- spec/rental_spec.rb
+++ spec/rental_spec.rb
@@ -6,4 +6,18 @@ describe Rental do
  before :each do
     @rentals = Rental.new 'Date', 'Book', 'Person'
   end
+
+
+  describe '#date' do
+    it 'returns the correct date' do
+      @rentals.date.should eql 'Date'
+    end
+  end
+
+    describe '#book' do
+    it 'returns the correct book' do
+      @book.book.should eql 'Book'
+    end
+  end
+
 end
